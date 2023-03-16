5398b59a42b45b6b3370fb930132249bdf478c13
Build Er <builder@macmini6.local>
2023-03-16 14:54:31 -0300

    Package version: 1.0.0-beta+20230316174639

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXFacebook",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXFacebook-1.0.0-beta+20230316142352.xcframework.zip",
-			checksum: "5fad86a710253c5a1c0fc2f0392d3606e8eb0cca74266d6057d3b87edfed80cb"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXFacebook-1.0.0-beta+20230316174639.xcframework.zip",
+			checksum: "1d8b38d3e04f1e8960e5f7ce8793d3404052f9b856375f70ca8eb8e3267e65c4"
 		)
 	]
 )
