42b6a51d6a1571aea2c42d322a5a19ad9a15d997
Build Er <builder@macmini6.local>
2023-04-18 10:15:46 -0300

    Package version: 1.0.0-beta+20230418130717

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
 		.target(name: "GXFacebookWrapper",
 				dependencies: [
 					"GXFacebook",
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .tvOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXFacebook",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXFacebook-1.0.0-beta+20230414101841.xcframework.zip",
-			checksum: "de1015e2138bc7ec46e90908a3e9fa59b1358cc12cf91d65247c0ae0a3bcad64"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXFacebook-1.0.0-beta+20230418130717.xcframework.zip",
+			checksum: "0d0633322751f3a3a44fc3b278538408719e36d8441e74a19b1bf0f6ae456631"
 		)
 	]
 )
