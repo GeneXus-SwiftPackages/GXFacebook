c14c448944d676b639327dd1cb0166123ef56900
genexusbot <builder@genexus.com>
2023-08-15 10:25:16 -0300

    Package version: 1.1.0

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXFacebook",
+	platforms: [.iOS("12.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "GXFacebook",
+			targets: ["GXFacebookWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0")
+	],
+	targets: [
+		.target(name: "GXFacebookWrapper",
+				dependencies: [
+					"GXFacebook",
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .tvOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXFacebook",
+			url: "https://pkgs.genexus.dev/iOS/releases/GXFacebook-1.1.0.xcframework.zip",
+			checksum: "58664c646f26b7b50b86623b2c4333db6f279ccefd8a73391c377f27d8a12876"
+		)
+	]
+)
