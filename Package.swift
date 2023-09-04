04647e8fe6a313e58cf66ad652e4ef7afcb3d586
Build Er <builder@macmini6.local>
2023-09-04 11:56:25 -0300

    Package version: 1.2.0-beta.20

--- Package.swift
+++ Package.swift
@@ -3,26 +3,26 @@ import PackageDescription
 
 let package = Package(
 	name: "GXFacebook",
-	platforms: [.iOS("12.0"), .tvOS("12.0")],
+	platforms: [.iOS("12.0")],
 	products: [
 		.library(
 			name: "GXFacebook",
 			targets: ["GXFacebookWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.2.0-beta.19")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.2.0-beta.20")
 	],
 	targets: [
 		.target(name: "GXFacebookWrapper",
 				dependencies: [
 					"GXFacebook",
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .tvOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXFacebook",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXFacebook-1.2.0-beta.19.xcframework.zip",
-			checksum: "2722248f7e7a52bff95a8905d08f072d2be19d57ed8e14429a3540fa8abdf1e1"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXFacebook-1.2.0-beta.20.xcframework.zip",
+			checksum: "4def22b5b89e1842f058c01fc0a0ea0fce51a755e42bf9cff3e726d6c55dab92"
 		)
 	]
 )
