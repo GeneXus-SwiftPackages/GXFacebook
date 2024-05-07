f85b6eb8d447d41b5e8b7000831d06c0f2849057
genexusbot <builder@genexus.com>
2024-05-07 16:13:41 -0300

    Package version: 1.5.16

--- Package.swift
+++ Package.swift
@@ -10,19 +10,23 @@ let package = Package(
 			targets: ["GXFacebookWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.5.12")
+		.package(url: "https://github.com/facebook/facebook-ios-sdk", .upToNextMinor(from: "17.0.1")),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.5.16")
 	],
 	targets: [
 		.target(name: "GXFacebookWrapper",
 				dependencies: [
 					"GXFacebook",
+					.product(name: "FacebookCore", package: "facebook-ios-sdk", condition: .when(platforms: [.iOS])),
+					.product(name: "FacebookLogin", package: "facebook-ios-sdk", condition: .when(platforms: [.iOS])),
+					.product(name: "FacebookShare", package: "facebook-ios-sdk", condition: .when(platforms: [.iOS])),
 					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXFacebook",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXFacebook-1.5.12.xcframework.zip",
-			checksum: "754ed06ccf5d848b00df7379e12c6a9ffa41f19432c37435b8c967a3f39a659e"
+			url: "https://pkgs.genexus.dev/iOS/releases/GXFacebook-1.5.16.xcframework.zip",
+			checksum: "23e08c3f0e583a2be1ccf3040924c739a85a1866662f9f43643213cd13f55e90"
 		)
 	]
 )
